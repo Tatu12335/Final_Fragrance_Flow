d9576ca6bdbcc0e9cb015fdb0f7431ea9eff8654
tatus <tatusolavi@gmail.com>
2026-02-10 16:12:11 +0200

    added the 1 api file createuser

---
+++ fragrance_API/Controllers/Fragrance_Controllers/CreateUser.cs
@@ -0,0 +1,43 @@
+﻿using Fragrance_flow_DL_VERSION_.interfaces;
+using Fragrance_flow_DL_VERSION_.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fragrance_API.Controllers.Fragrance_Controllers
+{
+    [ApiController]
+    [Route("api/Fragrance_Flow/Users")]
+    public class CreateUser : ControllerBase
+    {
+        private readonly IFragranceRepo _repo;
+        public CreateUser(IFragranceRepo repo)
+        {
+            _repo = repo;
+        }
+        /* public IActionResult Index()
+         {
+             return View();
+         }*/
+
+        [Route("api/Fragrance_Flow/Users/Create")]
+        [HttpPost]
+        public async Task<UserSession> Create(string username, string email, string password)
+        {
+            try
+            {
+                var userEntity = await _repo.CheckIfUserExists(username);
+
+                
+
+                var newUser = await _repo.CreateNewUserAsync(username, email, password);
+
+                return newUser;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            
+        }
+    }
+}
