c41ef5bb735a2539a09d21db0d9a3d2ec122afd6
tatus <tatusolavi@gmail.com>
2026-02-24 00:07:05 +0200

    Started working  on the admin functionality.

---
+++ fragrance_API/Controllers/AdminControllers/GetAllUsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fragrance_API.Controllers.AdminControllers
+{
+    public class GetAllUsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
