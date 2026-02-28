5202bc30df03842684741efbaf0dd15ecb58a742
tatus <tatusolavi@gmail.com>
2026-02-28 21:30:54 +0200

    Made the unban controller, took just a couple of minutes because i already have a repo that has all the methods i need.

---
+++ fragrance_API/Controllers/AdminControllers/UnbanUserController.cs
@@ -0,0 +1,31 @@
+﻿using Fragrance_flow_DL_VERSION_.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fragrance_API.Controllers.AdminControllers
+{
+    [Controller]
+    [Route("api/Fragrance_Flow/Users/Admin")]
+
+    public class UnbanUserController : Controller
+    {
+        public readonly IAdminServices _adminService;
+        public UnbanUserController(IAdminServices adminServices) 
+        { 
+            _adminService = adminServices;
+        }
+
+        [HttpPatch("Unban")]
+        public async Task<IActionResult> UnbanUserAsync([FromBody]int id)
+        {
+            try
+            {
+                await _adminService.UnbanUserById(id);
+                return Ok(new {message = " Successfully unbanned user" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($" Fatal error occured : {ex.Message}");
+            }
+        }
+    }
+}
